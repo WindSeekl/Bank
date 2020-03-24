86048cefb436380e7129e48712c396bb3cefe536
Wind <Wind@DESKTOP-SU0P5IG>
2020-03-24 15:19:34 +0800

    11

--- bank/src/com/neu/util/Pass.java
+++ bank/src/com/neu/util/Pass.java
@@ -4,4 +4,7 @@ public class Pass {
 	private static final String  account="WindSeekl";
 	private static final String  pass="n6A54C0sbnrio82b";
 	private static final String  data="2020.3.24";
+	public static void main(String[] args) {
+		System.out.println("报个锤子错");
+	}
 }
