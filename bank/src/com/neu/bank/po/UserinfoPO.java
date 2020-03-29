301bf664ad7790b9eefae7cc7f38c19454339a9e
EAandEX <1548466238@qq.com>
2020-03-29 20:16:31 +0800

    loginpull

---
+++ bank/src/com/neu/bank/po/UserinfoPO.java
@@ -0,0 +1,29 @@
+package com.neu.bank.po;
+
+public class UserinfoPO {
+private int userid;
+private String loginname;
+private String loginpass;
+
+
+public int getUserid() {
+	return userid;
+}
+public void setUserid(int userid) {
+	this.userid = userid;
+}
+public String getLoginname() {
+	return loginname;
+}
+public void setLoginname(String loginname) {
+	this.loginname = loginname;
+}
+public String getLoginpass() {
+	return loginpass;
+}
+public void setLoginpass(String loginpass) {
+	this.loginpass = loginpass;
+}
+
+
+}
---
+++ bank/src/com/neu/bank/service/login/loginService.java
@@ -0,0 +1,5 @@
+package com.neu.bank.service.login;
+
+public interface logins {
+	int logins(String Username ,String UserPass);
+}
