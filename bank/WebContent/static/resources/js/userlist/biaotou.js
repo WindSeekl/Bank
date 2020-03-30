6aa92ad6f7cc1d08d64403c835bdfb03177fbcad
EAandEX <1548466238@qq.com>
2020-03-30 14:02:10 +0800

    sta 框架

---
+++ bank/WebContent/static/resources/js/userlist/biaotou.js
@@ -0,0 +1,14 @@
+﻿document
+    .write('<table class="biaoge">'
+    + '<tr >'
+    + '<td id="liebiaotou" colspan="7"><span class="glyphicon glyphicon-calendar "></span><span>列表信息</span></td>'
+    + '</tr>'
+    + '<tr id="db">'
+    + '<td><input type="checkbox"></td>'
+    + ' <td>序号</td>'
+    + '<td>用户名</td>'
+    + '<td>真实姓名</td>'
+    + '<td>角色</td>'
+    + '<td>状态</td>'
+    + '<td class="tupianduiqi"><span class="glyphicon glyphicon-cog" ></span><span>操作</span></td>'
+    + '</tr>');
---
+++ bank/WebContent/static/resources/js/userlist/userlist.js
@@ -0,0 +1,17 @@
+﻿
+document
+    .write(
+        + '<tr class="danshu">'
+        + '<td><input type="checkbox"></td>'
+        + '<td></td>'
+        + '<td>DATE</td>'
+        + '<td>DATE</td>'
+        + '<td>DATE</td>'
+        + ' <td>DATE</td>'
+        + ' <td>'
+        + '<button class="glyphicon glyphicon-lock caozuo"></button>'
+        + '<button class="glyphicon glyphicon-pencil caozuo"></button>'
+        + '<button class="glyphicon glyphicon-trash caozuosc"></button>'
+        + ' </td></tr>'
+    );
+				
